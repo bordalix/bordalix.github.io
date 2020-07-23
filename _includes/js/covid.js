6284801e55103fb3bf57ba3c82563814cf04e61e
João Bordalo <joao.bordalo@gmail.com>
2020-07-23 10:08:06 +0100

    new employement

--- _includes/js/covid.js
+++ _includes/js/covid.js
@@ -24,12 +24,24 @@ const state = {
     'tosse', 'febre', 'dores_musculares', 'cefaleia',
     'fraqueza_generalizada', 'dificuldade_respiratoria',
   ],
-  employed: [
-    { month: 'Jan', total: 4815400, young: 295500, old: 4519900, active: 5213900 },
-    { month: 'Fev', total: 4803600, young: 290300, old: 4513300, active: 5213900 },
-    { month: 'Mar', total: 4793400, young: 290000, old: 4503400, active: 5213900 },
-    { month: 'Abr', total: 4750900, young: 268100, old: 4482800, active: 5213900 },
-    { month: 'Mai', total: 4662600, young: 240300, old: 4422400, active: 5213900 },
+  employment: [
+    { month: 'Jan19', young: 305500, old: 4502200, m: 2611600, f:2551100 },
+    { month: 'Fev19', young: 301900, old: 4511200, m: 2611400, f:2555400 },
+    { month: 'Mar19', young: 296800, old: 4521200, m: 2609000, f:2553500 },
+    { month: 'Abr19', young: 301000, old: 4537800, m: 2614600, f:2566000 },
+    { month: 'Mai19', young: 295700, old: 4559400, m: 2609300, f:2574200 },
+    { month: 'Jun19', young: 305700, old: 4562600, m: 2613800, f:2574400 },
+    { month: 'Jul19', young: 309900, old: 4569700, m: 2624100, f:2571400 },
+    { month: 'Ago19', young: 320200, old: 4566200, m: 2642200, f:2567500 },
+    { month: 'Set19', young: 312400, old: 4567800, m: 2637400, f:2582200 },
+    { month: 'Out19', young: 312400, old: 4558400, m: 2621300, f:2590700 },
+    { month: 'Nov19', young: 303600, old: 4544400, m: 2619100, f:2581300 },
+    { month: 'Dez19', young: 298900, old: 4518700, m: 2608200, f:2570200 },
+    { month: 'Jan20', young: 295500, old: 4519900, m: 2612100, f:2569200 },
+    { month: 'Fev20', young: 290300, old: 4513300, m: 2595500, f:2556100 },
+    { month: 'Mar20', young: 290000, old: 4503400, m: 2582900, f:2537600 },
+    { month: 'Abr20', young: 268100, old: 4482800, m: 2564800, f:2504700 },
+    { month: 'Mai20', young: 240300, old: 4422400, m: 2496300, f:2424200 },
   ],
   loading: 2, // loading state (from 2 to 0)
   toc: [],    // table of contents
@@ -708,66 +720,74 @@ const charts = {
       ],
     });
   },
-  empregados_activa: (outer) => {
-    createGraphContainer('empregados_activa', outer);
-    Highcharts.chart('empregados_activa', {
-      title: { text: 'Total população activa' },
-      yAxis: { title: { text: null }},
-      xAxis: {
-        categories: state.employed.map(i => i.month),
-      },
-      legend: { enable: false },
-      series: [{
-        name: 'População',
-        data: state.employed.map(i => i.active),
-      }],
-    });
-  },
-  empregados_total: (outer) => {
-    createGraphContainer('empregados_total', outer);
-    Highcharts.chart('empregados_total', {
-      title: { text: 'Total empregados' },
-      yAxis: { title: { text: null }},
-      xAxis: {
-        categories: state.employed.map(i => i.month),
-      },
-      legend: { enable: false },
-      series: [{
-        name: 'Empregados',
-        data: state.employed.map(i => i.total),
-      }],
-    });
-  },
-  empregados_menos24_total: (outer) => {
-    createGraphContainer('empregados_menos24_total', outer);
-    Highcharts.chart('empregados_menos24_total', {
-      title: { text: 'Total empregados 15 a 24 anos' },
-      yAxis: { title: { text: null }},
-      xAxis: {
-        categories: state.employed.map(i => i.month),
-      },
-      legend: { enable: false },
+  empregos_total: (outer) => {
+    createGraphContainer('empregos_total', outer);
+    Highcharts.chart('empregos_total', {
+      title:  { text: 'Total' },
+      xAxis:  { categories: state.employment.map(i => i.month) },
+      yAxis:  { title: { text: null }},
       series: [
         {
           name: 'Empregados',
-          data: state.employed.map(i => i.young),
+          data: state.employment.map(i => i.young + i.old)
         },
       ],
     });
   },
-  empregados_mais24_total: (outer) => {
-    createGraphContainer('empregados_mais24_total', outer);
-    Highcharts.chart('empregados_mais24_total', {
-      title: { text: 'Total empregados +24 anos' },
+  empregos_variacao: (outer) => {
+    series = [0];
+    state.employment.forEach((month, index) => {
+      if (index > 0) {
+        const prevMonth = state.employment[index-1];
+        const prevValue = prevMonth.young + prevMonth.old;
+        const thisValue = month.young + month.old;
+        series.push(thisValue - prevValue);
+      }
+    })
+    createGraphContainer('empregos_variacao', outer);
+    Highcharts.chart('empregos_variacao', {
+      chart:  { type: 'column' },
+      title:  { text: 'Variação mensal' },
+      xAxis:  { categories: state.employment.map(i => i.month) },
+      yAxis:  { title: { text: null }},
+      series: [
+        {
+          name: 'Empregados',
+          data: series,
+        },
+      ],
+    });
+  },
+  empregos_menos24_total: (outer) => {
+    createGraphContainer('empregos_menos24_total', outer);
+    Highcharts.chart('empregos_menos24_total', {
+      title: { text: 'Empregados 15 a 24 anos' },
       yAxis: { title: { text: null }},
       xAxis: {
-        categories: state.employed.map(i => i.month),
+        categories: state.employment.map(i => i.month),
       },
       legend: { enable: false },
       series: [
         {
           name: 'Empregados',
-          data: state.employed.map(i => i.old),
+          data: state.employment.map(i => i.young),
+        },
+      ],
+    });
+  },
+  empregos_mais24_total: (outer) => {
+    createGraphContainer('empregos_mais24_total', outer);
+    Highcharts.chart('empregos_mais24_total', {
+      title: { text: 'Empregado +24 anos' },
+      yAxis: { title: { text: null }},
+      xAxis: {
+        categories: state.employment.map(i => i.month),
+      },
+      legend: { enable: false },
+      series: [
+        {
+          name: 'Empregados',
+          data: state.employment.map(i => i.old),
         },
       ],
     });
@@ -854,10 +874,10 @@ function addGraphs() {
   outer = addLead('Evolução Rt');
   renderNewRt(outer);
   outer = addLead('Emprego');
-  charts['empregados_activa'](outer);
-  charts['empregados_total'](outer);
-  charts['empregados_menos24_total'](outer);
-  charts['empregados_mais24_total'](outer);
+  charts['empregos_total'](outer);
+  charts['empregos_variacao'](outer);
+  charts['empregos_menos24_total'](outer);
+  charts['empregos_mais24_total'](outer);
   renderTOC();
   document.getElementById('fontes').style.display = 'block';
 }
