499fa4370989e651ab0aa8cd09c7eb0be934b444
João Bordalo <joao.bordalo@gmail.com>
2020-12-14 16:11:10 +0000

    more code refactor

--- _includes/js/traffic.js
+++ _includes/js/traffic.js
@@ -1,26 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var jsonURL = 'https://joaobordalo.com/images/traffic/incidents.json?'
-              + new Date().getTime();
+  var jsonURL = 'https://joaobordalo.com/images/traffic/incidents.json?' + new Date().getTime();
   fetch(jsonURL)
-    .then(r => r.json())
-    .then(data => {
-      if (data.length > 0) {
-        document.getElementById('incidents-label').style.display = 'block';
-        var container = document.getElementById('incidents')
-        data.forEach(incident => {
-          var iconContainer = document.createElement('div');
-          iconContainer.innerHTML = '<p><i class="fa fa-warning"></i></p>';
-          var textContainer = document.createElement('div');
-          textContainer.innerHTML = '<p>' + incident.descricaoIncidencia + '</p>';
-          var div = document.createElement('div');
-          div.appendChild(iconContainer);
-          div.appendChild(textContainer);
-          container.appendChild(div);
-        });
-      }
-    });
-  const placeholder = 'https://cdn.joaobordalo.com/images'
-                    + '/static/traffic/placeholder.png';
+  .then(r => r.json())
+  .then(data => {
+    if (data.length > 0) {
+      document.getElementById('incidents-label').style.display = 'block';
+      var container = document.getElementById('incidents')
+      data.forEach(incident => {
+        var iconContainer = document.createElement('div');
+        iconContainer.innerHTML = '<p><i class="fa fa-warning"></i></p>';
+        var textContainer = document.createElement('div');
+        textContainer.innerHTML = '<p>' + incident.descricaoIncidencia + '</p>';
+        var div = document.createElement('div');
+        div.appendChild(iconContainer);
+        div.appendChild(textContainer);
+        container.appendChild(div);
+      });
+    }
+  });
+  const placeholder = 'https://cdn.joaobordalo.com/images/static/traffic/placeholder.png';
   ['south_cams', 'north_cams'].forEach(function (div_id) {
     var camIDs = {
       south_cams: [305, 306, 307, 5, 308, 309, 310, 311, 7, 312, 313, 149, 532, 314, 316],
@@ -28,15 +26,12 @@ document.addEventListener("DOMContentLoaded", function () {
     };
     var div = document.getElementById(div_id);
     camIDs[div_id].forEach(function (camID) {
-      var col = document.createElement('div');
-      col.classList.add('text_column');
       var url = 'https://joaobordalo.com/images/traffic/viaverde_' + camID + '.jpeg?' + new Date().getTime();
       var img = document.createElement('img');
       img.src = url;
       img.alt = 'Cam ID ' + camID;
       img.onerror = function() { img.src = placeholder; }
-      col.appendChild(img);
-      div.appendChild(col);
+      div.appendChild(img);
     });
   });
 });
